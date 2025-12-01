7f0cf3eccb9bc25aebdfdfdfd58a452d23acc1e1
신혜원 <hwkong7_@naver.com>
2025-12-01 14:07:14 +0900

    Initial Commit

---
+++ Tint/TintApp.swift
@@ -0,0 +1,17 @@
+//
+//  TintApp.swift
+//  Tint
+//
+//  Created by student on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TintTests/TintTests.swift
@@ -0,0 +1,17 @@
+//
+//  TintTests.swift
+//  TintTests
+//
+//  Created by student on 12/1/25.
+//
+
+import Testing
+@testable import Tint
+
+struct TintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
